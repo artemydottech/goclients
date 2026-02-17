8471f91a910ca6e7b08383c329f7e2364688743a
Tyoma <raskolnikoffweb@mail.ru>
2026-02-17 23:09:55 +0500

    feat: little features

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 	"os"
 
-	// Вот тут магия: имя_модуля/путь_к_папке
 	"github.com/artemydottech/goclients/internal/handlers"
 	"github.com/artemydottech/goclients/internal/repository"
 	"github.com/artemydottech/goclients/internal/service"
@@ -16,7 +15,6 @@ import (
 )
 
 func main() {
-	// 1. Загружаем переменные (как и было)
 	if err := godotenv.Load(); err != nil {
 		log.Println("Конфиг .env не найден")
 	}
@@ -26,31 +24,24 @@ func main() {
 		dbPath = "./data.db"
 	}
 
-	// 2. Инициализируем базу данных
 	db, err := sql.Open("sqlite3", dbPath)
 	if err != nil {
 		log.Fatal("Ошибка подключения к БД:", err)
 	}
 	defer db.Close()
 
-	// Создаем таблицы (можно оставить здесь для простоты, пока проект маленький)
 	_, err = db.Exec(`CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)`)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// 3. Собираем слои (Dependency Injection)
-	// ВАЖНО: Функции New... должны быть публичными (с большой буквы)
 	userRepo := repository.NewUserRepository(db)
 	userServ := service.NewUserService(userRepo)
-	userHdl  := handlers.NewUserHandler(userServ)
+	userHandlers  := handlers.NewUserHandler(userServ)
 
-	// 4. Настраиваем маршруты
 	mux := http.NewServeMux()
 	
-	// Теперь мы вызываем метод у созданного объекта хендлера
-	mux.HandleFunc("POST /users", userHdl.CreateUser)
-	// Сюда же добавишь потом GET /users и т.д.
+	mux.HandleFunc("POST /users", userHandlers.CreateUser)
 
 	port := os.Getenv("PORT")
 	if port == "" {
